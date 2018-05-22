a2916c6239d69e45b3d6ed17240e5c29d877c4fb
James Alastair McLaughlin <j.a.mclaughlin@ncl.ac.uk>
2018-05-22 23:26:18 +0100

    Refactor menus and add pushy (mobile) menu

---
+++ synbiohub/Menu.ts
@@ -0,0 +1,32 @@
+
+export default class Menu {
+
+    items:Array<MenuItem>
+
+    constructor() {
+        this.items = []
+    }
+
+    addItem(item:MenuItem) {
+        this.items.push(item)
+    }
+
+
+}
+
+export class MenuItem {
+    
+    title:string
+    url:string
+    faIconClass:string
+    subItems:Array<MenuItem>
+
+    constructor(title:string, url:string, faIconClass:string) {
+        this.title = title
+        this.url = url
+        this.faIconClass = faIconClass
+        this.subItems = []
+    }
+
+}
+
--- synbiohub/views/View.ts
+++ synbiohub/views/View.ts
@@ -1,15 +1,21 @@
 
 import { Request, Response } from 'express'
 import config from 'synbiohub/config'
+import Menu, { MenuItem } from 'synbiohub/Menu';
 
 export default abstract class View {
 
     config:any
     user:any
+    menu:Menu
 
     constructor() {
 
         this.config = config.get()
+
+        this.menu = new Menu()
+
+
 
     }
 
@@ -17,6 +23,16 @@ export default abstract class View {
 
         this.user = req.user
 
+        if(this.user) {
+            this.menu.addItem(new MenuItem('Submit', '/submit', 'fa-cloud-upload-alt'))
+            this.menu.addItem(new MenuItem('Shared with Me', '/shared', 'fa-bolt'))
+            this.menu.addItem(new MenuItem('Submissions', '/manage', 'fa-align-left'))
+            this.menu.addItem(new MenuItem('Admin', '/admin', 'fa-cogs'))
+            this.menu.addItem(new MenuItem('Profile', '/profile', 'fa-id-card'))
+            this.menu.addItem(new MenuItem('Logout', '/logout', 'fa-sign-out-alt'))
+        } else {
+            this.menu.addItem(new MenuItem('Login or Register', '/login', 'fa-sign-in'))
+        }
     }
 
     abstract async render(res:Response)
