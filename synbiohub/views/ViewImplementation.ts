d3c57add7d907158217d1ac42f0b2e0b0a904010
Christian <c.atallah2@newcastle.ac.uk>
2018-05-23 16:03:36 +0100

    got all the implementation information including provenance onto ViewImplementation

--- synbiohub/views/ViewImplementation.ts
+++ synbiohub/views/ViewImplementation.ts
@@ -1,6 +1,8 @@
 
 import sbolmeta = require('sbolmeta');
 import ViewTopLevelWithObject from 'synbiohub/views/ViewTopLevelWithObject';
+import DefaultSBOLFetcher from "../fetch/DefaultSBOLFetcher";
+import {getAttachmentsFromTopLevel} from 'synbiohub/attachments';
 
 import { Request, Response } from 'express'
 
@@ -13,6 +15,10 @@ export default class ViewImplementation extends ViewTopLevelWithObject {
 
     meta:any
 
+    agent:string
+    plan:string
+    location:string
+
     async prepare(req:Request) {
 
         await super.prepare(req)
@@ -22,9 +28,19 @@ export default class ViewImplementation extends ViewTopLevelWithObject {
         }
 
         this.setTopLevelMetadata(req, sbolmeta.summarizeGenericTopLevel(this.object))
+        
+        let activity_sbol = await DefaultSBOLFetcher.get(req).fetchSBOLObjectRecursive(this.meta.wasGeneratedBy.uri)
+        
+        let plan_uri = activity_sbol.object.associations[0].plan.uri.toString()
+        
+        let plan_sbol = await DefaultSBOLFetcher.get(req).fetchSBOLObjectRecursive(plan_uri)
+        
+        this.meta.description = this.meta.description.split('<br/>').join('')
+        this.location = this.annotations[2]['value']
+        this.agent = activity_sbol.object.associations[0].agent.name
+        this.plan = activity_sbol.object.associations[0].plan.name
 
-        console.log(this)
-        console.log('hithere')
+        this.meta.attachments = getAttachmentsFromTopLevel(plan_sbol, plan_sbol.object, req.url.toString().endsWith('/share'))
 
 
     }
