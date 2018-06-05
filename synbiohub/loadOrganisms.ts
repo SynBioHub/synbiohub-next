2630588648ec40861427b1d5fe15ac9383dd2656
Christian <c.atallah2@newcastle.ac.uk>
2018-06-05 15:02:01 +0100

    Made the organism typeahead use fmaprefix. updated gitignore.

--- synbiohub/loadOrganisms.ts
+++ synbiohub/loadOrganisms.ts
@@ -1,22 +1,28 @@
-let TrieSearch = require('trie-search')
+import FMAPrefix from './FMAPrefix'
+// let TrieSearch = require('trie-search')
 
-let organisms = require('/home/chris/Desktop/Dev/synbiohub-v2/faketax.json')
+// let organisms = require('/home/chris/Desktop/Dev/synbiohub-v2/faketax.json')
 
-let ts = new TrieSearch()
+// let ts = new TrieSearch()
     
-ts.addFromObject(organisms)
+// ts.addFromObject(organisms)
 
-ts.options.maxCacheSize=500
+// ts.options.maxCacheSize=500
 
-export default function(req, res){
+export default async function(req, res){
     
     let query = req.params.query
 
-    let matches = ts.get(query)
 
-    matches = JSON.stringify(matches.map((r) => r._key_))
+    let matches = await FMAPrefix.search('./alls.txt', query)
 
-    console.log(matches)
+    matches = matches.map((r) =>r.split('|')[0])
+
+    // let matches = ts.get(query)
+
+    // matches = JSON.stringify(matches.map((r) => r._key_))
+
+    // console.log(matches)
 
     res.send(matches)
 
