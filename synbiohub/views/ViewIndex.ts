343d7baa8211ab933e7401bbff6469609390a102
James Alastair McLaughlin <j.a.mclaughlin@ncl.ac.uk>
2018-08-28 21:12:48 +0100

    New homepage design part 1

--- synbiohub/views/ViewIndex.ts
+++ synbiohub/views/ViewIndex.ts
@@ -2,16 +2,42 @@
 import { SBHRequest } from "synbiohub/SBHRequest";
 import View from "synbiohub/views/View";
 import { Response } from 'express'
+import MDFetcherLocal from "../fetch/MDFetcherLocal";
+import uriToUrl from "synbiohub/uriToUrl";
+
+interface Project {
+    url:string
+    name:string
+}
 
 export default class ViewIndex extends View {
 
+    recentProjects:Array<Project>
+
     constructor() {
+
         super()
+
+        this.recentProjects = []
     }
 
     async prepare(req:SBHRequest) {
 
         await super.prepare(req)
+
+        if(req.user) {
+
+            let collections = await (new MDFetcherLocal(req.user.graphUri)).getRootCollectionMetadata()
+
+            for(let collection of collections) {
+
+                this.recentProjects.push({
+                    url: uriToUrl(collection.uri, req),
+                    name: collection.name
+                })
+            }
+
+        }
 
     }
 
