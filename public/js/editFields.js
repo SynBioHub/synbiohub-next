5fda9aef8dbb15ad6baa8738ea2238a21dbc4540
Christian <c.atallah2@newcastle.ac.uk>
2019-04-04 11:56:59 +0100

    Have made things more organised, and have made physical location in constructs editable

--- public/js/editFields.js
+++ public/js/editFields.js
@@ -4,16 +4,31 @@ $(document).ready(function() {
         if (this.id == 'plan_edit'){
 
             var plan = $("#planForm").attr('value')
-            
+
             $("#editablePlan").appendTo("#attachModalBody")
-            $("#attachModalBody").append('<input name="old_plan_id" id="old_plan_id" form="constructForm" type="hidden" value=' + plan + '>')
-            $("#attachModalBody").append('<input name="fieldType" id="fieldType" form="constructForm" type="hidden" value=plan >')
+            $("#attachModalBody").append('<input name="old_plan_id" id="old_plan_id" form="editPlanForm" type="hidden" value=' + plan + '>')
+            $("#attachModalBody").append('<input name="fieldType" id="fieldType" form="editPlanForm" type="hidden" value=plan >')
+
+        }
+
+        else if (this.id =="location_edit"){
+
+            var location = $("#location_edit").attr('value')
+
+            $("#editableLocation").appendTo("#attachModalBody")
+            $("#attachModalBody").append('<input name="old_location" id="old_location" form="editLocationForm" type="hidden" value="' + location +'">')
+            $("#attachModalBody").append('<input name="fieldType" id="fieldType" form="editLocationForm" type="hidden" value=location >')
+
+
 
         }
 
         $("#attachmentModal").on("hidden.bs.modal", function () {
 
             $("#editablePlan").appendTo("#allForms")
+
+            $("#editableLocation").appendTo("#allForms")
+            
             $("#attachModalBody").empty()
 
             });
--- synbiohub/actions/editField.ts
+++ synbiohub/actions/editField.ts
@@ -18,77 +18,114 @@ export default async function (req, res) {
 
         let type = fields["fieldType"][0]
 
-
         if (type === "plan"){
 
-            let old_plan_id = fields["old_plan_id"][0]
-            let old_plan_uri = uri.getURIPrefix() + old_plan_id + "/" + uri.getVersion()
-
-            let new_plan_uri
-
-
-            if (files['file'][0]['size'] == 0){
-                new_plan_uri = SBHURI.fromURIOrURL(fields["plan1"][0].split(',')[0])
-
-            }
-
-            else{
-                new_plan_uri = SBHURI.fromURIOrURL(uri.getURIPrefix() + fields["plan2"][0] + "/" + uri.getVersion())
-            }
-
-            let graph = new SBOL2Graph()
-
-            let plan = graph.createProvPlan(new_plan_uri.getURIPrefix(), new_plan_uri.getDisplayId(), new_plan_uri.getVersion())
-            plan.displayId = new_plan_uri.getDisplayId()
-            plan.name = new_plan_uri.getDisplayId()
+            editPlan(fields, files, uri)
             
-            let asc = graph.createProvAssociation(uri.getURIPrefix(), uri.getDisplayId() + '_association', uri.getVersion())
-
-            asc.plan = plan
-
-            let old_uri = SBHURI.fromURIOrURL(old_plan_uri)
-
-
-            var templateParams = {
-                subject:asc.uri,
-                predicate:"http://www.w3.org/ns/prov#hadPlan",
-                object:old_plan_uri
-            }
-
-            console.log(templateParams)
-
-            var removeQuery = loadTemplate('sparql/removeSpecificTriple.sparql', templateParams)
-
-            await sparql.deleteStaggered(removeQuery, old_uri.getGraph())
-
-            let uploader = new SBOLUploader()
-            uploader.setGraph(graph)
-            uploader.setDestinationGraphUri(new_plan_uri.getGraph())
-            uploader.setOverwriteMerge(OverwriteMergeOption.FailIfExists)
-
-            await uploader.upload()
-
-
-    
-
-
-            if (files['file'][0]['size'] != 0){
-
-
-                let fileStream = await fs.createReadStream(files['file'][0]['path']);
-                let uploadInfo = await uploads.createUpload(fileStream)
-                const { hash, size, mime } = uploadInfo
-                await attachments.addAttachmentToTopLevel(uri.getGraph(), uri.getURIPrefix() 
-                , new_plan_uri,
-                files['file'][0]['originalFilename'], hash, size, mime,
-                uri.getGraph().split('/').pop)
-            }
-
-
-            res.redirect(uri)
-            
-
         }
 
+        else if (type === "location"){
+            editLocation(fields, files, uri)
+        }
+
+        res.redirect(uri)
+
+}
+
+async function editPlan(fields, files, uri){
+
+    let old_plan_id = fields["old_plan_id"][0]
+    let old_plan_uri = uri.getURIPrefix() + old_plan_id + "/" + uri.getVersion()
+
+    let new_plan_uri
+
+
+    if (files['file'][0]['size'] == 0){
+        new_plan_uri = SBHURI.fromURIOrURL(fields["plan1"][0].split(',')[0])
+
+    }
+
+    else{
+        new_plan_uri = SBHURI.fromURIOrURL(uri.getURIPrefix() + fields["plan2"][0] + "/" + uri.getVersion())
+    }
+
+    let graph = new SBOL2Graph()
+
+    let plan = graph.createProvPlan(new_plan_uri.getURIPrefix(), new_plan_uri.getDisplayId(), new_plan_uri.getVersion())
+    plan.displayId = new_plan_uri.getDisplayId()
+    plan.name = new_plan_uri.getDisplayId()
+    
+    let asc = graph.createProvAssociation(uri.getURIPrefix(), uri.getDisplayId() + '_association', uri.getVersion())
+
+    asc.plan = plan
+
+    let old_uri = SBHURI.fromURIOrURL(old_plan_uri)
+
+
+    var templateParams = {
+        subject:asc.uri,
+        predicate:"http://www.w3.org/ns/prov#hadPlan",
+        object:old_plan_uri
+    }
+
+    console.log(templateParams)
+
+    var removeQuery = loadTemplate('sparql/removeSpecificURITriple.sparql', templateParams)
+
+    await sparql.deleteStaggered(removeQuery, old_uri.getGraph())
+
+    let uploader = new SBOLUploader()
+    uploader.setGraph(graph)
+    uploader.setDestinationGraphUri(new_plan_uri.getGraph())
+    uploader.setOverwriteMerge(OverwriteMergeOption.FailIfExists)
+
+    await uploader.upload()
+
+    if (files['file'][0]['size'] != 0){
+
+        let fileStream = await fs.createReadStream(files['file'][0]['path']);
+        let uploadInfo = await uploads.createUpload(fileStream)
+        const { hash, size, mime } = uploadInfo
+        await attachments.addAttachmentToTopLevel(uri.getGraph(), uri.getURIPrefix() 
+        , new_plan_uri,
+        files['file'][0]['originalFilename'], hash, size, mime,
+        uri.getGraph().split('/').pop)
+    }
+
+}
+
+async function editLocation(fields, files, uri){
+
+    let old_location = fields["old_location"][0]
+    let new_location = fields["location"][0]
+
+
+    let graph = new SBOL2Graph()
+
+    let impl = graph.createImplementation(uri.getURIPrefix(), uri.getDisplayId(), uri.getVersion())
+
+    impl.setStringProperty('http://wiki.synbiohub.org/wiki/Terms/synbiohub#physicalLocation', new_location)
+
+    console.log(graph.serializeXML())
+
+
+    var templateParams = {
+        subject:impl.uri,
+        predicate:"http://wiki.synbiohub.org/wiki/Terms/synbiohub#physicalLocation",
+        object:old_location
+    }
+
+    console.log(templateParams)
+
+    var removeQuery = loadTemplate('sparql/removeSpecificLiteralTriple.sparql', templateParams)
+
+    await sparql.deleteStaggered(removeQuery, uri.getGraph())
+
+    let uploader = new SBOLUploader()
+    uploader.setGraph(graph)
+    uploader.setDestinationGraphUri(uri.getGraph())
+    uploader.setOverwriteMerge(OverwriteMergeOption.FailIfExists)
+
+    await uploader.upload()
 
 }
